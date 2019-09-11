e7e70ca53e415a5e5e80399995277890cedd00fb
daemon.devin <daemon.devin@gmail.com>
2019-09-10 18:59:49 -0700

    Create generateAnchor.snippet.php

---
+++ generateAnchor.snippet.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * anchor
+ * 
+ * A Snippet to generate proper anchor tags which respect 
+ * current request parameters.
+ * 
+ * NOTE: Call this Snippet non-cacheable to ensure your 
+ * anchor tags are always generated with the current query 
+ * string parameters.
+ */
+ 
+$anchor = $modx->getOption('anchor',$scriptProperties,'');
+$title = $modx->getOption('title',$scriptProperties,'');
+$link = $modx->getOption('link',$scriptProperties,'');
+$cls = $modx->getOption('cls',$scriptProperties,''); 
+$output = '';
+$scheme = !isset($scheme) || $scheme === '' ? $modx->getOption('link_tag_scheme', $scriptProperties, 'full') : $scheme;
+
+$resourceId = $modx->resource->get('id');
+$queryString = $modx->request->getParameters();
+
+$url = $modx->makeUrl($resourceId, '', modX::toQueryString($queryString), $scheme) . '#' . urlencode(strtolower($anchor));
+
+$output  = '<a name="'.urlencode(strtolower($anchor)).'" ';
+if(!empty($cls)) {
+	$output .= 'class="'.$cls.'"';
+}
+if(!empty($link) && $link != false) {
+	$output .= 'href="'. $url .'">'. (empty($title)?$anchor:$title) .'</a>';
+} else { 
+	$output .= '></a>';
+}
+return $output;
