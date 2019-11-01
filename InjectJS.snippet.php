56a7e5b716e792a3e90f648b148beb5c48ea5334
daemon.devin <daemon.devin@gmail.com>
2019-11-01 15:56:15 -0700

    Create InjectJS.snippet.php

---
+++ InjectJS.snippet.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * ThemanticInjectJS.
+ *
+ * Register JS to be injected inside the HEAD tag or just
+ * before the closing BODY tag of a resource. Can be used
+ * as an output modifier or ran as a snippet call.
+ * This modifier expects the following parameters:
+ * which are as follows:
+ *
+ * &mod       boolean   If false, will run as a snippet call.
+ * &js        string    The JS to inject into the resource.
+ * &startup   boolean   If true, injects the JS between the
+ *                      HEAD tags, default is false which will
+ *                      insert the JS just before the closing
+ *                      BODY tag.
+ *
+ * @author  daemon.devin <daemon.devin@gmail.com>
+ * @link    https://github.com/daemondevin/MODx-Snippets
+ * @version 1.0
+ */
+$mod      = (boolean) $modx->getOption('mod',$scriptProperties,true);
+$js       = (string)  $modx->getOption('js',$scriptProperties,null);
+$startup  = (boolean) $modx->getOption('startup',$scriptProperties,false);
+
+$output = $mod === 'true' ? $input : $js;
+
+$startup === 'true' ? $modx->regClientStartupScript($output) : $modx->regClientScript($output);
+
+return;
