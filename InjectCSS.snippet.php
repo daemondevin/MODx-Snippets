f238ef1fa1cd6cbdda4838b14047dc74ced3b42a
daemon.devin <daemon.devin@gmail.com>
2019-11-24 23:32:36 -0800

    Update InjectCSS.snippet.php

--- InjectCSS.snippet.php
+++ InjectCSS.snippet.php
@@ -9,16 +9,23 @@
  *
  * &mod   boolean   If false, will run as a snippet call.
  * &css   string    The CSS to inject into the resource's HEAD tag.
+ * &media string    all, aural, braille, embossed, handheld, print, 
+ *                  projection, screen, tty, tv
  *
  * @author  daemon.devin <daemon.devin@gmail.com>
  * @link    https://github.com/daemondevin/MODx-Snippets
  * @version 1.0
  */
-$mod = (boolean) $modx->getOption('mod',$scriptProperties,'true');
-$css = (string)  $modx->getOption('css',$scriptProperties,null);
+$mod   = (boolean) $modx->getOption('mod',$scriptProperties,true);
+$css   = (string)  $modx->getOption('css',$scriptProperties,null);
+$media = (string)  $modx->getOption('media',$scriptProperties,null);
 
-$output = $mod === 'true' ? $input : $css;
+$output = $mod === true ? $input : $css;
 
-$modx->regClientCSS($output);
+if (!is_null($media)) {
+  $modx->regClientCSS($output, $media);
+} else {
+  $modx->regClientCSS($output, $media);
+}
 
 return;
